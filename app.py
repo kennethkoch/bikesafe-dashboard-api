b4c6568cd26ff507e6a143f39387066f187e469e
Kenny Koch <kennykoch47@gmail.com>
2024-01-02 14:46:42 -0500

    change port

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from app import app
 import os
 host='0.0.0.0'
-port = int(os.environ.get('PORT', 5000))
+port = int(os.environ.get('PORT', 8000))
 if __name__ == "__main__":
     app.run(host='localhost', port=port, debug=True)
